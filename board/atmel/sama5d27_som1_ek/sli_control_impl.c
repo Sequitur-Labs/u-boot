985f704f101e492d0413c7ded81640f5ded4d12d
vincent.ting <vincent.ting@seqlabs.com>
2020-02-13 23:55:27 -0800

    implementations for control and io

---
+++ board/atmel/sama5d27_som1_ek/sli_control_impl.c
@@ -0,0 +1,93 @@
+#include <common.h>
+
+#include <asm/io.h>
+
+#include "mach/sama5d2.h"
+#include "sli/sli_control.h"
+
+
+void sli_reset_board(void)
+{
+	writel(0xA5000005,ATMEL_BASE_SYSC);
+}
+
+#ifdef CONFIG_CORETEE_WATCHDOG
+/*
+ * AT91SAM9 watchdog runs a 12bit counter @ 256Hz,
+ * use this to convert a watchdog
+ * value from seconds.
+ */
+#define WDT_SEC2TICKS(s)	(((s) << 8) - 1)
+static void sli_setup_watchdog( void ){
+	u64 timeout;
+	u32 ticks;
+	u32 regval;
+
+	printf("Setting watchdog\n");
+
+	/* Calculate timeout in seconds and the resulting ticks */
+	timeout = 50000;
+	do_div(timeout, 1000);
+	timeout = min_t(u64, timeout, WDT_MAX_TIMEOUT);
+	ticks = WDT_SEC2TICKS(timeout);
+
+	regval=10000;
+	while(regval > 0) regval--;
+
+	regval = readl(ATMEL_BASE_WDT + AT91_WDT_MR);
+	printf("MR: 0x%08x\n", regval);
+	printf("Ticks: 0x%08x\n", ticks);
+
+	regval &= ~(AT91_WDT_MR_WDDIS);
+	writel(regval, ATMEL_BASE_WDT + AT91_WDT_MR);
+
+	regval=10000;
+	while(regval > 0) regval--;
+
+	/*
+	 * All counting occurs at SLOW_CLOCK / 128 = 256 Hz
+	 *
+	 * Since WDV is a 12-bit counter, the maximum period is
+	 * 4096 / 256 = 16 seconds.
+	 */
+	regval = AT91_WDT_MR_WDRSTEN	/* causes watchdog reset */
+		| AT91_WDT_MR_WDDBGHLT		/* disabled in debug mode */
+		| AT91_WDT_MR_WDD(0xfff)	/* restart at any time */
+		| AT91_WDT_MR_WDV(ticks);	/* timer value */
+
+	writel(regval, ATMEL_BASE_WDT + AT91_WDT_MR);
+
+	regval=10000;
+	while(regval > 0) regval--;
+
+	printf("Restart watchdog\n");
+	writel(AT91_WDT_CR_WDRSTT | AT91_WDT_CR_KEY, ATMEL_BASE_WDT + AT91_WDT_CR);
+
+	printf("Done watchdog\n");
+	return;
+}
+#endif
+
+//-----------------------------------------------
+//Reset Status Register
+#define RST_SR_ADDR 0xF8048004
+unsigned int sli_get_reset_cause(void)
+{
+	unsigned int regval=0;
+	unsigned int reason;
+	regval = readl(RST_SR_ADDR);
+	reason = (regval & 0x0700)>>8;
+	printf("Reset Cause: 0x%08x\n", reason);
+
+	switch (reason) {
+	case 0x00:
+	case 0x01:
+	case 0x02:
+		return SLI_RC_POR;
+	case 0x03:
+		return SLI_RC_WDOG;
+	default:
+		break;
+	}
+	return SLI_RC_UNKNOWN;
+}
---
+++ board/atmel/sama5d27_som1_ek/sli_io_impl.c
@@ -0,0 +1,114 @@
+#include <common.h>
+#include <compiler.h>
+#include <spi.h>
+#include <spi_flash.h>
+#include <dm/device-internal.h>
+
+#include "sli/sli_io.h"
+
+
+static struct spi_flash* _flash=0;
+
+static int initDevice(int device)
+{
+	unsigned int bus = CONFIG_SF_DEFAULT_BUS;
+	unsigned int cs = CONFIG_SF_DEFAULT_CS;
+	unsigned int speed = CONFIG_SF_DEFAULT_SPEED;
+	unsigned int mode = CONFIG_SF_DEFAULT_MODE;
+	int ret=0;
+#ifdef CONFIG_DM_SPI_FLASH
+	struct udevice *new, *bus_dev;
+	/* In DM mode defaults will be taken from DT */
+	speed = 0, mode = 0;
+
+	/* Remove the old device, otherwise probe will just be a nop */
+	ret = spi_find_bus_and_cs(bus, cs, &bus_dev, &new);
+	if (!ret) {
+		device_remove(new, DM_REMOVE_NORMAL);
+	}
+	_flash = NULL;
+	ret = spi_flash_probe_bus_cs(bus, cs, speed, mode, &new);
+	if (ret) {
+		printf("Failed to initialize SPI flash at %u:%u (error %d)\n",
+		       bus, cs, ret);
+		return 1;
+	}
+
+	_flash = dev_get_uclass_priv(new);
+#else
+	struct spi_flash *new;
+	if (_flash)
+		spi_flash_free(_flash);
+
+	new = spi_flash_probe(bus, cs, speed, mode);
+	_flash = new;
+
+	if (!new) {
+		printf("Failed to initialize SPI flash at %u:%u\n", bus, cs);
+		return 1;
+	}
+
+	_flash = new;
+#endif
+	return (_flash != NULL);
+}
+
+
+
+//-----------------------------------------------
+int sli_nvm_read(int device,size_t offset,size_t len,void* buffer)
+{
+	int res=-1;
+
+	if (initDevice(device))
+	{
+		// ignoring device for now
+		res=spi_flash_read(_flash,offset,len,buffer);
+	} else {
+		printf("Failed to initialize SPI device!!!\n");
+	}
+	return res;
+}
+
+
+int sli_nvm_write(int device,size_t offset,size_t len,void* buffer)
+{
+	int res=-1;
+
+	if (initDevice(device))
+	{
+		res=sli_nvm_erase(device,offset,len);
+		if (!res)
+		{
+			// ignoring device now
+			res=spi_flash_write(_flash,offset,len,buffer);
+		}
+	}
+	return res;
+}
+
+#define SLI_SPI_ERASE_SIZE 0x10000
+int sli_nvm_erase(int device,size_t offset,size_t len)
+{
+	int res=0;
+
+	if (initDevice(device))
+	{
+		/*
+		 * Can only erase SLI_SPI_ERASE_SIZE at a time on some boards
+		 */
+		uint32_t remaining = len;
+		uint32_t erasestart = offset - (offset%SLI_SPI_ERASE_SIZE);
+		while(res==0 && remaining > 0){
+			//printf("Calling erase on: 0x%08x, size: 0x%08x\n", erasestart, SLI_SPI_ERASE_SIZE);
+			res = spi_flash_erase(_flash, erasestart, SLI_SPI_ERASE_SIZE);
+			if(remaining > SLI_SPI_ERASE_SIZE) {
+				erasestart += SLI_SPI_ERASE_SIZE;
+				remaining -= SLI_SPI_ERASE_SIZE;
+			} else {
+				remaining = 0;
+			}
+		}
+	}
+	return res;
+}
