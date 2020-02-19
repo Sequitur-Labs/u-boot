86ef441f19ebe21cd3e5c6351654c83bb9f4b365
vincent.ting <vincent.ting@seqlabs.com>
2020-02-19 15:12:30 -0800

    typo in diversification message

--- common/spl/sli/sli_prov.c
+++ common/spl/sli/sli_prov.c
@@ -219,7 +219,7 @@ static int stage_2(void)
 		diversifyComponent(PLEX_ID_B_STR,"uboot","Plex B: U-Boot");
 		diversifyComponent(PLEX_ID_B_STR,"linux","Plex B: Linux Kernel");
 		diversifyComponent(PLEX_ID_B_STR,"dtb","Plex B: Device Tree Binary");
-		diversifyComponent(PLEX_ID_B_STR,"initramfs","Plex A: initramfs");
+		diversifyComponent(PLEX_ID_B_STR,"initramfs","Plex B: initramfs");
 
 		//Certs are handled by Coretee.
 
